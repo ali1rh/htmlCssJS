10c88c785cfee6bec733506dbb343d704df1b3df
AliRezeHeidari <h118ar@gmail.com>
2022-05-15 22:26:14 +0430

    fontDana

---
+++ style.js
@@ -0,0 +1,21 @@
+// $("#tab-1").click(function (e) {
+//     $("#content-1").css({
+//         "opacity": "100%",
+//         "z-index": "100"
+//     })
+// });
+
+// $("#tab-2").click(function (e) {
+//     $("#content-2").css({
+//         "opacity": "1",
+//         "z-index": "100"
+//     })
+//     console.log('#tab-2');
+// });
+// $("#tab-3").click(function (e) {
+//     $("#content-3").css({
+//         "opacity": "1",
+//         "z-index": "100"
+//     })
+//     console.log('#tab-3');
+// });
